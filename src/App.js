19d6a572bf5f0618be7f7ead609c32dede5b1395
Thibaut <thibaut.guibert@edu.escp.eu>
2020-11-06 19:00:12 +0100

    cleaned the components  to start the dojo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Pokedex from './components/Pokedex';
-
 
 function App() {
   return (
     <div className="App">
       <p>Let start the dojo!</p>
-      <Pokedex />
     </div>
   );
 }
